96a18dc4ca11af1b62190540b59c470bd7c20757
Drishti Regmi <dregmi@ucsd.edu>
2024-06-29 00:49:04 -0700

    committing updated memorize UI from scratch

---
+++ MemorizeFromScratch/MemorizeFromScratch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemorizeFromScratch
+//
+//  Created by Drishti Regmi on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemorizeFromScratch/MemorizeFromScratch/MemorizeFromScratchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeFromScratchApp.swift
+//  MemorizeFromScratch
+//
+//  Created by Drishti Regmi on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeFromScratchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
